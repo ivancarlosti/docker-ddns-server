ec7c7bd3550714d6f99de7ad76bc3a84f2fbd3b9
Ivan Carlos <ivan@ivancarlos.com.br>
2024-05-27 14:43:24 -0300

    Update host.go

--- dyndns/model/host.go
+++ dyndns/model/host.go
@@ -6,6 +6,18 @@ import (
 	"gorm.io/gorm"
 )
 
+// Host is a dns host entry.
+type Host struct {
+	gorm.Model
+	Hostname   string    `gorm:"unique_index:idx_host_domain;not null" form:"hostname" validate:"required,hostname"`
+	Domain     string    `gorm:"unique_index:idx_host_domain;not null" form:"domain" validate:"required,hostname"`
+	Ip         string    `form:"ip" validate:"omitempty,ipv4|ipv6"`
+	Ttl        int       `form:"ttl" validate:"required,min=20,max=86400"`
+	LastUpdate time.Time `form:"lastupdate"`
+	UserName   string    `gorm:"unique" form:"username" validate:"min=3"`
+	Password   string    `form:"password" validate:"min=8"`
+}
+
 // UpdateHost updates all fields of a host entry
 // and sets a new LastUpdate date.
 func (h *Host) UpdateHost(updateHost *Host) (updateRecord bool) {
